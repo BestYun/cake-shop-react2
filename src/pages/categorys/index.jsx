d85fdc4169dd7777f76b203ac8a6906579d04857
yun <lixingyunxing@163.com>
2019-12-07 23:31:21 +0800

    no message

--- src/pages/categorys/index.jsx
+++ src/pages/categorys/index.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 import './index.css'
+import { Tabs, WhiteSpace, Badge } from 'antd-mobile';
+
+const tabs = [
+    { title: '蛋糕' },
+    { title: '面包' },
+    { title: '下午茶' },
+    { title: '饮品' },
+];
+
+
+
 
 export default function CategoryPage() {
+
+    console.log('---CategoryPage---')
     return (
-        <div>
-            <p>CategoryPage page</p>
-            <p>顶部标签分类</p>
-        </div>
+        <div data-flex='dir:top' style={{ height: '100%' }}>
+            <Tabs tabs={tabs}
+                initialPage={0}
+                onChange={(tab, index) => { console.log('onChange', index, tab); }}
+                onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+                tabBarUnderlineStyle={{
+                    height: '6px',
+                    backgroundColor: '#f74b60',
+                    color: '#f74b60',
+                    borderWidth: 0,
+
+                }}
+                tabBarActiveTextColor={
+                    '#f74b60'
+                }
+            >
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', backgroundColor: '#fff' }}>
+                    Content of first tab
+                    </div>
+            </Tabs>
+
+        </div >
     )
 }
