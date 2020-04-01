e39c16a9d593c5799c1fd69b9fc68bd6864801a3
yun <lixingyunxing@163.com>
2020-04-01 14:26:06 +0800

    no message

---
+++ src/pages/me/address.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AddressPage() {
+    return (
+        <div data-flex='dir:top'>
+            <div data-flex='dir:left'
+            >地址管理</div>
+        </div>
+    )
+}
